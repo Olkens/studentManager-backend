9d191ef364c04fab6744b341451d357980eed34e
olkens <olkusz.michal@gmail.com>
2022-12-18 00:04:19 +0100

    init

---
+++ src/main/java/com/example/wers/MercuryApplication.java
@@ -0,0 +1,13 @@
+package com.example.wers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercuryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercuryApplication.class, args);
+	}
+
+}
